43653c1a3b0fa442a9745d011c232983870a0e23
Rutvik-M <manthalkar.rutvik@gmail.com>
2026-06-20 11:20:48 +0530

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashmap={}
+        for words in strs:
+            word=[0]*26
+            for ch in words:
+                word[ord(ch)-ord("a")]+=1
+            key=tuple(word)
+            if key not in hashmap:
+                hashmap[key]=[]
+            hashmap[key].append(words)
+        return list(hashmap.values())
+
+        
